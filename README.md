66bb495160a03920f1bb0339d2c38e2d945b98c1
Exikron <charfe.walid@gmail.com>
2017-07-04 12:20:54 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,49 @@
+Dashboard - Socialize US - Install
+========================
+
+The events will display on a specific view, reachable on another repository -- https://github.com/echopen-groupe-9/front-Vuejs. The separation in two repository beetween our main site and this dashboard has for main purpose to keep clear, and to help you, install this without any issue.
+
+Install
+--------------
+Clone the repository, then go to the project cd MyPathToTheFolder and do
+```
+composer install
+
+php bin/console doctrine:database:create
+
+php bin/console assets:install --symlink
+
+php bin/console doctrine:schema:update --dump-sql --force
+
+php bin/console server:run
+```
+If you want to create an admin user directly from the CLI :
+```
+php bin/console fos:user:create adminuser --super-admin
+```
+Otherwhise just go to ```/register```
+Finnaly, in order to access to the dashboard simply add ```/dashboard``` in the url parameter. 
+
+
+Note that in order to work properly it will create a database. Make sure your mandatory information to connect SQL server are correct in the parameters.yml
+```
+Echopen/
+├── app/
+│   ├── config
+│   │   ├── parameters.yml
+```
+Here is a sample of what this file should look like with a connexion in localhost
+```
+parameters:
+    database_host: localhost # 127.0.0.1
+    database_port: null
+    database_name: echopen # the name that will have your database
+    database_user: root
+    database_password: root 
+    mailer_transport: smtp
+    mailer_host: 127.0.0.1
+    mailer_user: null
+    mailer_password: null
+    secret: # your personal token 
+
+```
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ * @Vich\Uploadable
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * NOTE: This is not a mapped field of entity metadata, just a simple property.
+     *
+     * @Vich\UploadableField(mapping="image", fileNameProperty="imageName")
+     * @var File
+     * )
+     */
+    private $imageFile;
+
+    /**
+     * @ORM\Column(name="image_name", type="string", length=255, nullable=true)
+     *
+     * @var string
+     */
+    private $imageName;
+
+    /**
+     * @ORM\Column(type="datetime",nullable=true)
+     * @var \DateTime
+     */
+    private $updatedAt;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+    }
+
+    public function setImageFile(File $image = null)
+    {
+        $this->imageFile = $image;
+
+        // VERY IMPORTANT:
+        // It is required that at least one field changes if you are using Doctrine,
+        // otherwise the event listeners won't be called and the file is lost
+        if ($image) {
+            // if 'updatedAt' is not defined in your entity, use another property
+            $this->updatedAt = new \DateTime('now');
+        }
+    }
+
+    public function getImageFile()
+    {
+        return $this->imageFile;
+    }
+
+    /**
+     * Set imageName
+     *
+     * @param string $imageName
+     *
+     * @return Image
+     */
+    public function setImageName($imageName)
+    {
+        $this->imageName = $imageName;
+
+        return $this;
+    }
+
+    /**
+     * Get imageName
+     *
+     * @return string
+     */
+    public function getImageName()
+    {
+        return $this->imageName;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     *
+     * @return Newchapter
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+}
